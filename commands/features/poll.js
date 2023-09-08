9b317e41f52171fd37eeaf8a0002dddd90266a89
ArshdeepGadhri <arshdeep.gadhri@mail.utoronto.ca>
2023-09-07 21:56:58 -0400

    updated poll command

--- commands/features/poll.js
+++ commands/features/poll.js
@@ -7,24 +7,40 @@ module.exports = {
         .setDescription("Create a poll")
         .setDMPermission(false)
         .setDefaultMemberPermissions(PermissionFlagsBits.Administrator)
-        .addStringOption(option =>
-            option
-                .setName("question")
-                .setDescription("Poll question")
-                .setRequired(true)),
-    /**
-     * 
-     * @param {ChatInputCommandInteraction} interaction 
-     */
+        .addSubcommand(subcommand =>
+            subcommand
+                .setName('default')
+                .setDescription('Create a yes/no poll')
+                .addStringOption(option => option.setName("question").setDescription("Poll question").setRequired(true)))
+        .addSubcommand(subcommand =>
+            subcommand
+                .setName('custom')
+                .setDescription('Create a custom poll')
+                .addStringOption(option => option.setName("question").setDescription("Poll question").setRequired(true))
+                .addStringOption(option => option.setName("optionone").setDescription("First option").setRequired(true))
+                .addStringOption(option => option.setName("optiontwo").setDescription("Second option").setRequired(true))),
+/**
+ * 
+ * @param {ChatInputCommandInteraction} interaction 
+ */
     async execute(interaction) {
         const question = interaction.options.getString("question")
 
+        var optionOne = "Yes"
+        var optionTwo = "No"
+        if (interaction.options.getSubcommand() === 'custom') {
+            optionOne = interaction.options.getString("optionone") || optionOne;
+            optionTwo = interaction.options.getString("optiontwo") || optionTwo;
+        }
+
         const pollEmbed = new EmbedBuilder()
-            .setDescription(`**Question**\n ${question}`)
+            .setDescription(`**${question}**`)
             .addFields([
-                { name: "Yes", value: "0", inline: true },
-                { name: "No", value: "0", inline: true }
+                { name: optionOne, value: "0", inline: true },
+                { name: optionTwo, value: "0", inline: true }
             ])
+            .setTimestamp()
+            .setThumbnail("https://w7.pngwing.com/pngs/842/509/png-transparent-opinion-polling-on-the-donald-trump-administration-poll-everywhere-social-polling-youtube-youtube-text-trademark-rectangle-thumbnail.png")
             .setColor('Aqua');
 
         const replyObject = await interaction.reply({ embeds: [pollEmbed], fetchReply: true });
@@ -32,13 +48,13 @@ module.exports = {
         const pollButtons = new ActionRowBuilder()
             .addComponents(
                 new ButtonBuilder()
-                    .setLabel("Yes")
+                    .setLabel(optionOne)
                     .setCustomId(`Poll-Yes-${replyObject.id}`)
-                    .setStyle(ButtonStyle.Success),
+                    .setStyle(ButtonStyle.Primary),
                 new ButtonBuilder()
-                    .setLabel("No")
+                    .setLabel(optionTwo)
                     .setCustomId(`Poll-No-${replyObject.id}`)
-                    .setStyle(ButtonStyle.Danger)
+                    .setStyle(ButtonStyle.Secondary)
             )
 
         interaction.editReply({ components: [pollButtons] })
