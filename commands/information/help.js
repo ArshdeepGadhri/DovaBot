cf527989b845b167162e9402029f182d7e9db23a
ArshdeepGadhri <arshdeep.gadhri@mail.utoronto.ca>
2023-09-09 04:45:58 -0400

    help command embed thumbnail

--- commands/information/help.js
+++ commands/information/help.js
@@ -50,6 +50,7 @@ module.exports = {
         const embed = new EmbedBuilder()
             .setDescription("See lists of commands by selecting a category down below!")
             .setColor("DarkButNotBlack")
+            .setThumbnail("https://cutewallpaper.org/24/help-icon-png/peer-help-vector-icons-free-download-in-svg-png-format.png")
             .setAuthor({ name: `${client.user.username}'s Commands`, iconURL: client.user.avatarURL() })
 
         const components = (state) => [
@@ -91,6 +92,7 @@ module.exports = {
                 .setTitle(`${emojis[directory.toLowerCase()] || null} ${formatString(directory)} commands`)
                 .setDescription(`A list of all the commands categorized under ${directory}`)
                 .setColor("DarkButNotBlack")
+                .setThumbnail("https://cutewallpaper.org/24/help-icon-png/peer-help-vector-icons-free-download-in-svg-png-format.png")
                 .addFields(
                     category.commands.map(i => {
                         return {
@@ -107,93 +109,5 @@ module.exports = {
         collector.on("end", () => {
             initialMessage.edit({ components: components(true), ephemeral: true });
         });
-
-
-        // var adminEmail = process.env.ADMIN_EMAIL;
-        // var thumbnailURL = interaction.client.user.displayAvatarURL(); 
-        // const embed = new EmbedBuilder()
-        //     .setColor("DarkGold")
-        //     .setTitle("Help (Page 1)")
-        //     .setDescription("Help command guide")
-        //     .addFields(
-        //         { name: "Page 1", value: "Help & Resources (Page 1)" },
-        //         { name: 'Page 2', value: "Information Commands (Page 2)" },
-        //         { name: 'Page 3', value: "Feature Commands (Page 3)" },
-        //         { name: '\u200B', value: '\u200B' },
-        //         { name: "Admin email contact", value: adminEmail }
-        //     )
-        //     .setThumbnail(thumbnailURL)
-        //     .setTimestamp();
-
-        // const embed2 = new EmbedBuilder()
-        //     .setColor("DarkGold")
-        //     .setTitle("Information Commands (Page 2)")
-        //     .addFields(
-        //         { name: "/calendar", value: "Do /calendar to retrieve the calendar" },
-        //         { name: '/faq', value: "Do /faq to list out the most frequently asked questions with answers" },
-        //         { name: '/info', value: "Do /info to get information about a user or the server" },
-        //         { name: '/private', value: "Do /private to create a private channel to speak to one of the admins" },
-        //         { name: '/schedule', value: "Do /schedule and provide a day to get the schedule for that day" },
-        //         { name: '/update (get)', value: "Do /update (get) to retrieve the latest set updates by an admin" }
-        //     )
-        //     .setThumbnail(thumbnailURL)
-        //     .setTimestamp();
-
-        // const embed3 = new EmbedBuilder()
-        //     .setColor("DarkGold")
-        //     .setTitle("Feature Commands (Page 3)")
-        //     .addFields(
-        //         { name: "/clear", value: "Do /clear to bulk remove messages" },
-        //         { name: '/newsletter', value: "Do /newsletter to retrieve the newsletter" },
-        //         { name: '/ping', value: "Do /ping to check for the bot's latency" },
-        //         { name: '/poll', value: "Do /poll to create a y/n poll" },
-        //         { name: '/thread', value: "Do /thread to create a new thread" }
-        //     )
-        //     .setThumbnail(thumbnailURL)
-        //     .setTimestamp();
-
-        // const button = new ActionRowBuilder()
-        //         .addComponents(
-        //             new ButtonBuilder()
-        //                 .setCustomId("page1")
-        //                 .setLabel("Page 1")
-        //                 .setStyle(ButtonStyle.Secondary),
-
-        //             new ButtonBuilder()
-        //                 .setCustomId("page2")
-        //                 .setLabel("Page 2")
-        //                 .setStyle(ButtonStyle.Secondary),
-
-        //             new ButtonBuilder()
-        //                 .setCustomId("page3")
-        //                 .setLabel("Page 3")
-        //                 .setStyle(ButtonStyle.Secondary),
-        //         )
-
-        // const message = await interaction.reply({ embeds: [embed] , components: [button], ephemeral: true})
-        // const collector = await message.createMessageComponentCollector();
-
-        // collector.on("collect", async i => {
-        //     if (i.customId === 'page1') {
-        //         if (i.user.id !== interaction.user.id) {
-        //             return;
-        //         }
-        //         await i.update({ embeds: [embed], components: [button], ephemeral: true })
-        //     }
-
-        //     if (i.customId === 'page2') {
-        //         if (i.user.id !== interaction.user.id) {
-        //             return;
-        //         }
-        //         await i.update({ embeds: [embed2], components: [button], ephemeral: true })
-        //     }
-
-        //     if (i.customId === 'page3') {
-        //         if (i.user.id !== interaction.user.id) {
-        //             return;
-        //         }
-        //         await i.update({ embeds: [embed3], components: [button], ephemeral: true })
-        //     }
-        // })
     },
 };
