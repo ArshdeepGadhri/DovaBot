9caa57a86c69ae3eb9abd4ba50a6e2790dc2cf34
ArshdeepGadhri <arshdeep.gadhri@mail.utoronto.ca>
2023-09-10 00:56:37 -0400

    Changes to reminder command to pm the user instead

--- commands/features/remindme.js
+++ commands/features/remindme.js
@@ -8,12 +8,6 @@ module.exports = {
     data: new SlashCommandBuilder()
         .setName('remindme')
         .setDescription('Reminds you of a note')
-        .addChannelOption(option =>
-            option
-                .setName("channel")
-                .setDescription("Channel to send the message in")
-                .setRequired(true)
-        )
         .addStringOption(option =>
             option
                 .setName("time")
@@ -23,14 +17,13 @@ module.exports = {
         .addStringOption(option =>
             option
                 .setName("message")
-                .setDescription('The message itself')
+                .setDescription('The message to send')
                 .setRequired(true)
         ),
 
     async execute(interaction) {
         const { default: ms } = await import("ms");
 
-        var channel = interaction.options.getChannel("channel");
         var time = ms(interaction.options.getString("time") || '1m');
         var message = interaction.options.getString("message");
 
@@ -43,7 +36,7 @@ module.exports = {
             .setTimestamp(true);
 
         setTimeout(() => {
-            channel.send({ content: `<@${interaction.member.user.id}>`, embeds: [embed] });
+            interaction.member.user.send({ embeds: [embed] });
         }, time); 
 
         await interaction.reply({ content: "Your reminder has been noted", ephemeral: true })
